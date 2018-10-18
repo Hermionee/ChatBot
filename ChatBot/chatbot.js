f707f931e333b7b86ba9cf1fc886efba9c8ca0e7
Elly <danyang.he@tum.de>
2018-10-17 20:41:09 -0400

    Add files via upload

---
+++ ChatBot/chatbot.js
@@ -0,0 +1,38 @@
+<script type="text/javascript" src="lib/axios/dist/axios.standalone.js"></script>
+<script type="text/javascript" src="lib/CryptoJS/rollups/hmac-sha256.js"></script>
+<script type="text/javascript" src="lib/CryptoJS/rollups/sha256.js"></script>
+<script type="text/javascript" src="lib/CryptoJS/components/hmac.js"></script>
+<script type="text/javascript" src="lib/CryptoJS/components/enc-base64.js"></script>
+<script type="text/javascript" src="lib/url-template/url-template.js"></script>
+<script type="text/javascript" src="lib/apiGatewayCore/sigV4Client.js"></script>
+<script type="text/javascript" src="lib/apiGatewayCore/apiGatewayClient.js"></script>
+<script type="text/javascript" src="lib/apiGatewayCore/simpleHttpClient.js"></script>
+<script type="text/javascript" src="lib/apiGatewayCore/utils.js"></script>
+<script type="text/javascript" src="apigClient.js"></script>
+var apigClient = apigClientFactory.newClient();
+var params = {
+    //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
+    param0: '',
+    param1: ''
+};
+var body = {
+    //This is where you define the body of the request
+};
+var additionalParams = {
+    //If there are any unmodeled query parameters or headers that need to be sent with the request you can add them here
+    headers: {
+        param0: '',
+        param1: ''
+    },
+    queryParams: {
+        param0: '',
+        param1: ''
+    }
+};
+
+apigClient.methodName(params, body, additionalParams)
+    .then(function(result){
+        //This is where you would put a success callback
+    }).catch( function(result){
+        //This is where you would put an error callback
+    });
